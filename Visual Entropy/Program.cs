5d9141d7595004db9b6531e36ebcf3c57f708902
Christopher Robert Philabaum <chaos7theory@live.com>
2013-11-05 23:17:48 -0700

    Added RGBa manipulation functions.

--- Visual Entropy/Program.cs
+++ Visual Entropy/Program.cs
@@ -20,7 +20,7 @@ namespace Visual_Entropy
 			{
 				for (int y = 0; y < height; y++)
 				{
-					Console.Write("{0}", ColorTranslator.ToHtml(Color.FromArgb(pixels[x,y])));
+					Console.Write("#{0}", Color.FromArgb(pixels[x,y]).Name);
 					if (y < height - 1) Console.Write(",");
 				}
 				Console.WriteLine();
@@ -32,6 +32,7 @@ namespace Visual_Entropy
 		}
 
 
+
 		public static int[,] Get_Pixels(Random rand, int width, int height, bool isAlpha = false)
 		{
 			// Width, Height, RGBa
@@ -40,15 +41,79 @@ namespace Visual_Entropy
 			{
 				for (int y = 0; y < height; y++)
 				{
-					pixels[x,y] = rand.Next();
-					/*if (!isAlpha)
-					{
-						colors[] = 255;
-					}*/
+					byte b = Convert.ToByte((uint)rand.Next() >> 24);
+					pixels[x, y] = 0;
+					pixels[x, y] = !isAlpha ? Set_Alpha(pixels[x, y], 255) : Set_Alpha(pixels[x,y], b);
+					pixels[x, y] = Set_Red(pixels[x, y], b);
+					pixels[x, y] = Set_Green(pixels[x, y], b);
+					pixels[x, y] = Set_Blue(pixels[x, y], b);
 				}
 			}
 
 			return pixels;
+		}
+
+
+
+		public static byte[] Get_RGBa(int color)
+		{
+			// Red, Green, Blue, Alpha
+			byte[] cBytes = new byte[4];
+			cBytes[0] = Convert.ToByte((color >> 16) & 0x0FF);
+			cBytes[1] = Convert.ToByte((color >> 8) & 0x0FF);
+			cBytes[2] = Convert.ToByte(color & 0x0FF);
+			cBytes[3] = Convert.ToByte((color >> 24) & 0x0FF);
+
+			return cBytes;
+		}
+
+		public static byte[] Get_RGBa(Color color)
+		{
+			return Get_RGBa(color.ToArgb());
+		}
+
+		public static int Set_Red(int color, byte red)
+		{
+			byte[] cBytes = Get_RGBa(color);
+			color = Convert.ToInt32(cBytes[3]); // Alpha
+			color = Convert.ToInt32((color << 8) + red);
+			color = Convert.ToInt32((color << 8) + cBytes[1]); // Green
+			color = Convert.ToInt32((color << 8) + cBytes[2]); // Blue
+
+			return color;
+		}
+
+		public static int Set_Green(int color,  byte green)
+		{
+			byte[] cBytes = Get_RGBa(color);
+			color = Convert.ToInt32(cBytes[3]); // Alpha
+			color = Convert.ToInt32((color << 8) + cBytes[0]);
+			color = Convert.ToInt32((color << 8) + green); // Green
+			color = Convert.ToInt32((color << 8) + cBytes[2]); // Blue
+
+			return color;
+		}
+
+		public static int Set_Blue(int color,  byte blue)
+		{
+			byte[] cBytes = Get_RGBa(color);
+			color = Convert.ToInt32(cBytes[3]); // Alpha
+			color = Convert.ToInt32((color << 8) + cBytes[0]); // Red
+			color = Convert.ToInt32((color << 8) + cBytes[1]); // Green
+			color = Convert.ToInt32((color << 8) + blue); // Blue
+
+			return color;
+		}
+
+		public static int Set_Alpha(int color, byte alpha)
+		{
+			byte[] cBytes = Get_RGBa(color);
+			color = Convert.ToInt32(alpha);
+			color = Convert.ToInt32((color << 8) + cBytes[0]); // Red
+			color = Convert.ToInt32((color << 8) + cBytes[1]); // Green
+			color = Convert.ToInt32((color << 8) + cBytes[2]); // Blue
+
+			return color;
 		}
 	}
 }
